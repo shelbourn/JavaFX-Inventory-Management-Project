50c0d83919016dbd8dbf0fad80ff7b0f24c23435
shelbourn <matthew@shelbourn.io>
2020-06-01 13:47:44 -0700

    Restructuring file tree

--- src/Main/InventoryManager.java
+++ src/Main/InventoryManager.java
@@ -1,3 +1,5 @@
+package Main;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,7 +20,7 @@ public class InventoryManager extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML_Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_Main.fmxl"));
         
         Scene scene = new Scene(root);
         
