b641c4af6064ed7c4bdd470bcad156b7d41e59b0
shelbourn <matthew@shelbourn.io>
2020-06-02 16:12:44 -0700

    Building out main screen.

--- src/Controller/MainScreenController.java
+++ src/Controller/MainScreenController.java
@@ -10,7 +10,13 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -20,7 +26,53 @@ import javafx.scene.input.MouseEvent;
 public class MainScreenController implements Initializable {
 
     @FXML
-    private Button button1;
+    private AnchorPane mainScrProdPane;
+    @FXML
+    private TableView<?> mainScrPartTab1;
+    @FXML
+    private TableColumn<?, ?> mainScrProdIDCol;
+    @FXML
+    private TableColumn<?, ?> mainScrProdNameCol;
+    @FXML
+    private TableColumn<?, ?> mainScrInvLvlProdCol;
+    @FXML
+    private TableColumn<?, ?> mainScrPriceCol;
+    @FXML
+    private Label mainScrProdLbl;
+    @FXML
+    private Button mainScrProdAddBtn;
+    @FXML
+    private Button mainScrProdModifyBtn;
+    @FXML
+    private Button mainScrProdDeleteBtn;
+    @FXML
+    private Button mainScrProdSearchBtn;
+    @FXML
+    private TextField mainScrPartSearchField;
+    @FXML
+    private AnchorPane mainScrPartPane;
+    @FXML
+    private TableView<?> mainScrPartTab;
+    @FXML
+    private TableColumn<?, ?> mainScrPartIDCol;
+    @FXML
+    private TableColumn<?, ?> mainScrPartNmCol;
+    @FXML
+    private TableColumn<?, ?> mainScrInvLvlPartCol;
+    @FXML
+    private TableColumn<?, ?> mainScrCostCol;
+    @FXML
+    private Label mainScrPartsLbl;
+    @FXML
+    private Button mainScrPartSearchBtn;
+    @FXML
+    private Button mainScrPartAddBtn;
+    @FXML
+    private Button mainScrPartModifyBtn;
+    @FXML
+    private Button mainScrPartDeleteBtn;
+    @FXML
+    private Button mainScrExitBtn;
 
     /**
      * Initializes the controller class.
@@ -31,7 +83,47 @@ public class MainScreenController implements Initializable {
     }    
 
     @FXML
-    private void button1ClickHandler(MouseEvent event) {
+    private void mainScrProdAddBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrProdModifyBtn(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrProdDeleteBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrProdSearchBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrProdSearchFieldHandler(InputMethodEvent event) {
+    }
+
+    @FXML
+    private void mainScrPartSearchBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrPartSearchFieldHandler(InputMethodEvent event) {
+    }
+
+    @FXML
+    private void mainScrPartAddBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrPartModifyBtn(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrPartDeleteBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void mainScrExitBtnHandler(MouseEvent event) {
     }
     
 }
