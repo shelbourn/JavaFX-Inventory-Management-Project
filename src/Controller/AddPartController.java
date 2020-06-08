0be4caba46512233c7f7ef1bd90ddedc48be72d7
shelbourn <matthew@shelbourn.io>
2020-06-08 13:42:00 -0700

    Completed v1 of 'Add Part' scr.

--- src/Controller/AddPartController.java
+++ src/Controller/AddPartController.java
@@ -7,7 +7,16 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,6 +25,39 @@ import javafx.fxml.Initializable;
  */
 public class AddPartController implements Initializable {
 
+    @FXML
+    private Button mainScrExitBtn;
+    @FXML
+    private AnchorPane addPartScrPane;
+    @FXML
+    private Label mainScrPartsLbl;
+    @FXML
+    private RadioButton addPartScrIHRadio;
+    @FXML
+    private RadioButton addPartScrOutRadio;
+    @FXML
+    private Button addPartScrSaveBtn;
+    @FXML
+    private Button addPartScrCancelBtn;
+    @FXML
+    private ToggleGroup addPartScrRadioBtns;
+    @FXML
+    private Label addPartScrMachIDLbl;
+    @FXML
+    private TextField addPartScrMachIDField;
+    @FXML
+    private TextField addPartScrIDField;
+    @FXML
+    private TextField addPartScrPartPriceField;
+    @FXML
+    private TextField addPartScrPartMaxField;
+    @FXML
+    private TextField addPartScrPartNameField;
+    @FXML
+    private TextField addPartScrPartInvField;
+    @FXML
+    private TextField addPartScrPartMinField;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +65,54 @@ public class AddPartController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void mainScrExitBtnHandler(MouseEvent event) {
+    }
+
+
+    @FXML
+    private void addPartScrIHRadioHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addPartScrOutRadioHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addPartScrSaveBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addPartScrCancelBtnHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addPartScrMachIDFieldHandler(KeyEvent event) {
+    }
+
+    @FXML
+    private void addPartScrIDFieldHandler(KeyEvent event) {
+    }
+
+    @FXML
+    private void addPartScrPartPriceFieldHandler(KeyEvent event) {
+    }
+
+    @FXML
+    private void addPartScrPartMaxFieldHandler(KeyEvent event) {
+    }
+
+    @FXML
+    private void addPartScrPartNameFieldHandler(KeyEvent event) {
+    }
+
+    @FXML
+    private void addPartScrPartInvFieldHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addPartScrPartMinFieldHandler(KeyEvent event) {
+    }
     
 }
