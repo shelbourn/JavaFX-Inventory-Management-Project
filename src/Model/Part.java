085b7ccff60dcf04742b84dd10ef8b95a0aa755d
shelbourn <matthew@shelbourn.io>
2020-06-16 14:54:26 -0700

    Adding variables, getters, and setter for Part

--- src/Model/Part.java
+++ src/Model/Part.java
@@ -9,4 +9,67 @@ package Model;
 
 public abstract class Part {
     
+    // Variables
+    private int partID;
+    private String partName;
+    private double partPrice;
+    private int partStockLevel;
+    private int minStockLevelPart;
+    private int maxStockLevelPart;
+
+    // Getter and Setter for partID
+    public int getPartID() {
+        return partID;
+    }
+
+    public void setPartID(int partID) {
+        this.partID = partID;
+    }
+
+    // Getter and Setter for partName
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    // Getter and Setter for partPrice
+    public double getPrice() {
+        return partPrice;
+    }
+
+    public void setPrice(double partPrice) {
+        this.partPrice = partPrice;
+    }
+
+    // Getter and Setter for partStockLevel
+    public int getStockLevel() {
+        return partStockLevel;
+    }
+
+    public void setStockLevel(int partStockLevel) {
+        this.partStockLevel = partStockLevel;
+    }
+
+    // Getter and Setter for minStockLevelPart
+    public int getMinStockLevel() {
+        return minStockLevelPart;
+    }
+
+    public void setMinStockLevel(int minStockLevelPart) {
+        this.minStockLevelPart = minStockLevelPart;
+    }
+
+    // Getter and Setter for maxStockLevelPart
+    public int getMaxStockLevel() {
+        return maxStockLevelPart;
+    }
+
+    public void setMaxStockLevel(int maxStockLevelPart) {
+        this.maxStockLevelPart = maxStockLevelPart;
+    }
+    
+    
 }
