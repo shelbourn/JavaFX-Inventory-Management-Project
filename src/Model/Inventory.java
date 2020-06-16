01f0c4e73c3dec063fb481230dd58fa1beb2edbf
shelbourn <matthew@shelbourn.io>
2020-06-15 18:01:11 -0700

    Building out Inventory class

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Matthew Shelbourn <mshelbo@wgu.edu>
+ * 
+ * Enables adding, deleting, modifying, and searching for parts and products in inventory.
  */
+
 package Model;
 
-/**
- *
- * @author Matthew Shelbourn <mshelbo@wgu.edu>
+/***
+ * JavaFX package imports to enable observable array lists
  */
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
 public class Inventory {
+    /***
+     * Variables
+     */
     
+    private ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private ObservableList<Product> allProducts = FXCollections.observableArrayList();
+  
+    /***
+     * Getters
+     */
+    
+    public ObservableList<Part> getParts(){
+        return allParts;
+    }
+    
+    public ObservableList<Product> getProducts(){
+        return allProducts;
+    }
+    
+    /***
+     * Methods
+     */
+    
+    /***
+     * Add Part
+     * @param newPart 
+     */
+    
+    public void addPart (Part newPart) {
+        allParts.add(newPart);
+    }
+    
+    /***
+     * Add Product
+     * @param newProduct 
+     */
+    
+    public void addProduct (Product newProduct){
+        allProducts.add(newProduct);
+    }
+    
+    public Part lookupPart(int partId){
+        for (Part p : allParts){
+            p.
+        }
+    }
 }
+
+
