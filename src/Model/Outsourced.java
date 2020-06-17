6e2bd290c77f52e10cc72d8e40d5b74636484535
shelbourn <matthew@shelbourn.io>
2020-06-17 16:21:23 -0700

    Finished Outsourced Part sub-class

--- src/Model/Outsourced.java
+++ src/Model/Outsourced.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Model;
-
 /**
- *
+ * Defines the properties and methods for the Outsourced class which is a sub-class of the
+ * Part super class
+ * 
  * @author Matthew Shelbourn <mshelbo@wgu.edu>
  */
-public class Outsourced {
+
+package Model;
+
+// Package Imports
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Outsourced extends Part {
     
+    // Properties for Outsource sub-class
+    private StringProperty companyName;
+    
+    // Constructor to Instantiate Outsourced Part sub-class
+    public Outsourced() {
+        super();
+        this.companyName = new SimpleStringProperty();
+    }
+
+    // Getter for Outsourced Part properties
+    public final StringProperty getCompanyName() {
+        return companyName;
+    }
+
+    // Setter for Outsourced Part properties
+    public final void setCompanyName(StringProperty companyName) {
+        this.companyName = companyName;
+    }
 }
