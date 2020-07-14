faaee853661b245ead45687d7be7459699618da4
shelbourn <matthew@shelbourn.io>
2020-07-14 16:00:00 -0700

    Building out Main Screen controller.

--- src/Controller/MainScreenController.java
+++ src/Controller/MainScreenController.java
@@ -5,12 +5,18 @@
  */
 package Controller;
 
+import Model.InHouse;
+import Model.Inventory;
+import Model.Part;
+import Model.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -23,6 +29,7 @@ import javafx.scene.layout.AnchorPane;
  */
 public class MainScreenController implements Initializable {
 
+    // FXML Created Properties
     @FXML
     private AnchorPane mainScr;
     @FXML
@@ -72,51 +79,113 @@ public class MainScreenController implements Initializable {
     @FXML
     private Button partsDeleteBtn;
 
+    // Additional properties required for functionality
+    private static Part partModify;
+    private static int partModifyIndex;
+    private static Product productModify;
+    private static int productModifyIndex;
+
+    // Getters for additional private properties
+    public static int getPartModifyIndex() {
+        return partModifyIndex;
+    }
+
+    public static int getProductModifyIndex() {
+        return productModifyIndex;
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    // Method to create part table test data (******COMMENT OUT BEFORE FINAL BUILD*******)
+    private void partTableTestDataHelper() {
+        for (int i = 3000; i < 3006; i++) {
+            InHouse inHouseTest = new InHouse();
+            inHouseTest.setPartID(i);
+            inHouseTest.setPartName("Test" + i);
+            inHouseTest.setPartStockLevel(i - 2095);
+            inHouseTest.setPartPrice(i - 999);
+            Inventory.addPart(inHouseTest);
+        }
+    }
+
+    // Method to create product table test data (******COMMENT OUT BEFORE FINAL BUILD*******)
+    private void productTableTestDataHelper() {
+        for (int i = 2006; i > 2000; i--) {
+            Product productTest = new Product();
+            productTest.setProductID(i);
+            productTest.setProductName("Test" + i);
+            productTest.setProductStockLevel(i - 2095);
+            productTest.setProductPrice(i - 999);
+            Inventory.addProduct(productTest);
+        }
+    }
+
+    /**
+     * * Helper methods for updating table views
+     *
+     * @param event
+     */
+    private void updatePartsTable() {
+        partsTable
+    }
 
     @FXML
     private void exitBtnHandler(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("CONFIRMATION: EXIT APPLICATION");
+        alert.setHeaderText("Are you sure you would like to exit this application?");
+        alert.setContentText("Click OK to exit the application. \n\nClick CANCEL to continue and return to the current screen.");
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.OK) {
+            System.out.println("User confirmed. Exiting application. END.");
+            System.exit(0);
+        } else {
+            System.out.println("User cancelled alert. Returning to Main Screen.");
+            alert.close();
+        }
+    }
+}
+
+@FXML
+        private void productsAddBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void productsAddBtnHandler(ActionEvent event) {
+        private void productsModifyBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void productsModifyBtnHandler(ActionEvent event) {
+        private void productsDeleteBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void productsDeleteBtnHandler(ActionEvent event) {
+        private void productsSearchBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void productsSearchBtnHandler(ActionEvent event) {
+        private void partsSearchBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void partsSearchBtnHandler(ActionEvent event) {
+        private void partsAddBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void partsAddBtnHandler(ActionEvent event) {
+        private void partsModifyBtnHandler(ActionEvent event) {
     }
 
     @FXML
-    private void partsModifyBtnHandler(ActionEvent event) {
-    }
-
-    @FXML
-    private void partsDeleteBtnHandler(ActionEvent event) {
+        private void partsDeleteBtnHandler(ActionEvent event) {
     }
 
 
 
-    
+
 }
