8e49b9c4b005d25eebc875a852c858af8d693b94
shelbourn <matthew@shelbourn.io>
2020-07-16 00:10:15 -0700

    Building out ModifyProduct controller.

--- src/Controller/ModifyProductController.java
+++ src/Controller/ModifyProductController.java
@@ -5,8 +5,15 @@
  */
 package Controller;
 
+import static Controller.MainScreenController.getProductModifyIndex;
+import Model.InHouse;
+import Model.Inventory;
+import Model.Part;
+import Model.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,6 +30,7 @@ import javafx.scene.layout.AnchorPane;
  */
 public class ModifyProductController implements Initializable {
 
+    // FXML Created Properties
     @FXML
     private AnchorPane modifyProdScr;
     @FXML
@@ -44,15 +52,15 @@ public class ModifyProductController implements Initializable {
     @FXML
     private TextField minLevelField;
     @FXML
-    private TableView<?> addTable;
+    private TableView<Part> addTable;
     @FXML
-    private TableColumn<?, ?> addTablePartIDCol;
+    private TableColumn<Part, Integer> addTablePartIDCol;
     @FXML
-    private TableColumn<?, ?> addTablePartNameCol;
+    private TableColumn<Part, String> addTablePartNameCol;
     @FXML
-    private TableColumn<?, ?> addTableInvLevelCol;
+    private TableColumn<Part, Integer> addTableInvLevelCol;
     @FXML
-    private TableColumn<?, ?> addTablePPUCol;
+    private TableColumn<Part, Double> addTablePPUCol;
     @FXML
     private Button addBtn;
     @FXML
@@ -60,25 +68,94 @@ public class ModifyProductController implements Initializable {
     @FXML
     private TextField searchField;
     @FXML
-    private TableView<?> deleteTable;
+    private TableView<Part> deleteTable;
     @FXML
-    private TableColumn<?, ?> deleteTablePartIDCol;
+    private TableColumn<Part, Integer> deleteTablePartIDCol;
     @FXML
-    private TableColumn<?, ?> deleteTablePartNameCol;
+    private TableColumn<Part, String> deleteTablePartNameCol;
     @FXML
-    private TableColumn<?, ?> deleteTableInvLevelCol;
+    private TableColumn<Part, Integer> deleteTableInvLevelCol;
     @FXML
-    private TableColumn<?, ?> deleteTablePPUCol;
+    private TableColumn<Part, Double> deleteTablePPUCol;
     @FXML
     private Button deleteBtn;
+
+    // Additional properties required for functionality
+    private int productID;
+    private int productModifyIndex = getProductModifyIndex();
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
+    private String productFieldException = new String();
+    private String productValueException = new String();
+    private String productDataTypeException = new String();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        //Fetching and populating data for product to modify
+        Product productToModify = Inventory.getAllProducts().get(productModifyIndex);
+        productID = Inventory.getAllProducts().get(productModifyIndex).getProductID();
+        productIDField.setText("Auto-Generated:   " + productID);
+        productNameField.setText(productToModify.getProductName());
+        inventoryLevelField.setText(Integer.toString(productToModify.getProductStockLevel()));
+        priceField.setText(Double.toString(productToModify.getProductPrice()));
+        maxLevelField.setText(Integer.toString(productToModify.getProductMaxStockLevel()));
+        minLevelField.setText(Integer.toString(productToModify.getProductMinStockLevel()));
+
+        // Fetching and populating product's associated parts
+        associatedParts = productToModify.getAssociatedParts();
+
+        // Fetching and setting Add Table rows
+        addTablePartIDCol.setCellValueFactory(cellData -> cellData.getValue().getPartIDProperty().asObject());
+        addTablePartNameCol.setCellValueFactory(cellData -> cellData.getValue().getPartNameProperty());
+        addTableInvLevelCol.setCellValueFactory(cellData -> cellData.getValue().getPartStockLevelProperty().asObject());
+        addTablePPUCol.setCellValueFactory(cellData -> cellData.getValue().getPartPriceProperty().asObject());
+
+        // Fetching and setting Delete Table rows
+        deleteTablePartIDCol.setCellValueFactory(cellData -> cellData.getValue().getPartIDProperty().asObject());
+        deleteTablePartNameCol.setCellValueFactory(cellData -> cellData.getValue().getPartNameProperty());
+        deleteTableInvLevelCol.setCellValueFactory(cellData -> cellData.getValue().getPartStockLevelProperty().asObject());
+        deleteTablePPUCol.setCellValueFactory(cellData -> cellData.getValue().getPartPriceProperty().asObject());
+
+        // Initializing Add Part and Delete Part table views
+        updateAddTable();
+        updateDeleteTable();
+        testDataHelper();
+    }
+
+    // Helper Methods for Updating Table Views
+    // Method to create test data (******COMMENT OUT BEFORE FINAL BUILD*******)
+    private void testDataHelper() {
+        for (int i = 3000; i < 3006; i++) {
+            InHouse inHouseTest = new InHouse();
+            inHouseTest.setPartID(i);
+            inHouseTest.setPartName("Test" + i);
+            inHouseTest.setPartStockLevel(i - 2095);
+            inHouseTest.setPartPrice(i - 999);
+            Inventory.addPart(inHouseTest);
+        }
+    }
+
+    private void updateAddTable() {
+        addTable.setItems(Inventory.getAllParts());
+    }
+
+    private void updateDeleteTable() {
+        deleteTable.setItems(associatedParts);
+    }
+
+    @FXML
+    private void searchBtnHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void addBtnHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteBtnHandler(ActionEvent event) {
+    }
 
     @FXML
     private void saveBtnHandler(ActionEvent event) {
@@ -88,17 +165,4 @@ public class ModifyProductController implements Initializable {
     private void cancelBtnHandler(ActionEvent event) {
     }
 
-    @FXML
-    private void addBtnHandler(ActionEvent event) {
-    }
-
-    @FXML
-    private void searchBtnHandler(ActionEvent event) {
-    }
-
-    @FXML
-    private void deleteBtnHandler(ActionEvent event) {
-    }
-
-    
 }
