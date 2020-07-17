78af1a3c3ac488719e933774ce12d30b89b47d9a
shelbourn <matthew@shelbourn.io>
2020-07-16 21:52:12 -0700

    Refactoring model files. :recycle:

--- src/Model/InHouse.java
+++ src/Model/InHouse.java
@@ -1,10 +1,9 @@
 /**
  * Defines the properties and methods for the InHouse class which is a sub-class of the
  * Part super class
- * 
+ *
  * @author Matthew Shelbourn <mshelbo@wgu.edu>
  */
-
 package Model;
 
 // Package Imports
@@ -12,19 +11,24 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class InHouse extends Part {
-    
+
     // Properties for the InHouse Part sub-class
     private IntegerProperty machineID;
-    
+
     // Constructor to Instantiate InHouse Part sub-class
     public InHouse() {
         super();
         this.machineID = new SimpleIntegerProperty();
     }
 
-    // Getter for InHouse Part properties
+    // Getter for InHouse Part property values
     public int getMachineID() {
         return machineID.get();
+    }
+
+    // Getter for InHouse Part properties
+    public IntegerProperty getMachineIDProperty() {
+        return machineID;
     }
 
     // Setter for InHouse Part properties
